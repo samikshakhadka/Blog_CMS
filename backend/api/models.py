3733ccaf22bc0cbefaf2883c795f39473d0e13e2
Samiksha <swabhimankhadka2001@gmail.com>
2024-04-23 13:32:08 +0545

    feat(backend): token serilization implementation and create api for crud

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
+
+    def __str__(self):
+        return self.title
